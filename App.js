139e514b0f483b1b5527d79fe69910eb69ed5c0c
jjcello <76487259+jjcello@users.noreply.github.com>
2022-03-25 11:38:28 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Canvas from './js/canvas.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas></Canvas>
+    </div>
+  );
+}
+
+export default App;
